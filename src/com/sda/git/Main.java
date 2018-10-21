bfab00bfe5db25e594a45b9d08ea64710bf44950
TomaszStartek <tomaszstartek1987@gmail.com>
2018-10-21 12:55:11 +0200

    dodanie metod

--- src/com/sda/git/Main.java
+++ src/com/sda/git/Main.java
@@ -3,6 +3,22 @@ package com.sda.git;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        CustomList<String> lista = new CustomList<>();
+        lista.add("Jan");
+        lista.add("Maciej");
+        lista.add("Maciej");
+        lista.add("Maciej");
+        lista.add("Maciej");
+        lista.add("Maciej");
+        lista.add("Maciej");
+        lista.add("Maciej");
+        lista.add("Maciej");
+        lista.add("Maciej");
+        lista.add("Maciej");
+        lista.add("Maciej");
+        lista.add("Maciej");
+        for (int i=0;i<lista.size();i++) {
+            System.out.println(lista.get(i));
+        }
     }
 }
